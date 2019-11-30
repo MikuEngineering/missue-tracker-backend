67edcef07a086fd5c97038a3f9e4e9b6fa82ae29
virtualprism <alan159753@gmail.com>
2019-11-30 13:16:37 +0800

    Create the connection to MySQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+const {
+  DB_HOST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_DATABASE,
+} = process.env;
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DB_HOST,
+      port: 3306,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
 export class AppModule {}
