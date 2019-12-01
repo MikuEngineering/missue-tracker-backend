8aa1b8a9cc57329d27ee06f216e9c4805bb28133
Pin Lin <moneycat711@gmail.com>
2019-12-02 02:08:40 +0800

    Basic code for registering user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterUserDto } from './dto/register_user.dto';
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+    @Post()
+    async register(@Body() registerUserDto: RegisterUserDto) {
+        console.log(registerUserDto);
+        await this.usersService.register(registerUserDto);
+    }
+
+}
