d7f922406452d1cc798799a0040180398bb89aea
virtualprism <alan159753@gmail.com>
2019-12-06 17:35:07 +0800

    Store user id in the session and make it as an identifier
    Instead of storing a whole user object into the session,
    I use user id as the identifier in the session,
    because it is faster to compare an user id than a whole user object.
    
    Therefore, I also wrote a method called 'findOneById' in the class UsersService.

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SessionController } from './session.controller';
 import { SessionSerializer } from './session.serializer';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [SessionController],
   providers: [SessionSerializer],
   exports: [SessionSerializer]
--- src/session/session.serializer.ts
+++ src/session/session.serializer.ts
@@ -1,17 +1,36 @@
 import { PassportSerializer } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { ValidateUserDto } from '../auth/dto/validate_user.dto';
 
-type DoneSerialization = (err: Error, user: any) => void;
+type DoneSerialization = (err: Error, id: number) => void;
 
-type DoneDeserialization = (err: Error, payload: string) => void;
+type DoneDeserialization = (err: Error, payload: ValidateUserDto) => void;
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  serializeUser(user: any, done: DoneSerialization) {
-    done(null, user);
+  constructor(private readonly usersService: UsersService) {
+    super();
   }
 
-  deserializeUser(payload: any, done: DoneDeserialization) {
-    done(null, payload);
+  serializeUser(user: ValidateUserDto, done: DoneSerialization) {
+    done(null, user.id);
+  }
+
+  deserializeUser(id: number, done: DoneDeserialization) {
+    this.usersService.findOneById(id)
+      .then((user) => {
+
+        if (!user) {
+          done(new Error(`Unable to find the user with id ${id}`), null);
+          return;
+        }
+
+        // Filter out the password since we don't need it.
+        const { password, ...result } = user;
+
+        done(null, result);
+
+      });
   }
 }
