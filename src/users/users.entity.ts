9c4710c56f7a9352549c5d49cdf1d221d98e1d1f
virtualprism <alan159753@gmail.com>
2019-12-06 11:11:50 +0800

    Make username unique

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -6,7 +6,7 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ length: 180 })
+    @Column({ length: 180, unique: true })
     username: string;
 
     @Column()
