87227069ae535721b1ef385073cc196b0f626bd5
virtualprism <alan159753@gmail.com>
2019-12-13 00:43:35 +0800

    Remove a unused import

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
 import { Project } from '../projects/projects.entity';
