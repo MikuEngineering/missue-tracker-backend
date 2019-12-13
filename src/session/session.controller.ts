8ca74c1223d653283b5654dcb6dfc19482045942
virtualprism <alan159753@gmail.com>
2019-12-13 13:51:09 +0800

    Make the response body of the route login empty

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Post, Delete, UseGuards, Request } from '@nestjs/common';
+import { Request as ExpressRequest } from 'express';
 import { LoginGuard } from '../common/guards/login.guard';
 
 @Controller('session')
 export class SessionController {
   @UseGuards(LoginGuard)
   @Post()
-  async login(@Request() req) {
-    return req.user
+  async login() {
   }
 
   @Delete()
-  async logout(@Request() req) {
+  async logout(@Request() req: ExpressRequest) {
     req.logout();
   }
 }
