75c04e907c2a326411c95f97da51d0af937e2ea5
Pin Lin <moneycat711@gmail.com>
2019-12-15 16:36:00 +0800

    Replace auth failed status code to 401

--- src/common/guards/authenticated.guard.ts
+++ src/common/guards/authenticated.guard.ts
@@ -1,9 +1,18 @@
-import { ExecutionContext, Injectable, CanActivate } from '@nestjs/common';
+import { ExecutionContext, Injectable, CanActivate, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { SessionController } from '../../session/session.controller';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    return request.isAuthenticated();
+
+    if (request.isAuthenticated()) {
+      return true;
+    }
+
+    const classSignature = context.getClass();
+    const method = context.getHandler();
+
+    throw new UnauthorizedException();
   }
 }
