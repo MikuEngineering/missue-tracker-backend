4a14e1fe4800802f7fd215dfd3aa9de8731f9045
Pin Lin <PinLin@users.noreply.github.com>
2019-12-15 20:18:54 +0800

    Revert "Create a route for project creation"

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './projects.entity';
-import { ProjectsController } from './projects.controller';
-import { ProjectsService } from './projects.service';
-import { TagsModule } from '../tags/tags.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), TagsModule],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
+  imports: [ TypeOrmModule.forFeature([Project]) ],
 })
 export class ProjectsModule {}
