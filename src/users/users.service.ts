c8af636d0d4e0950eccd9e244414bebd049a2992
Pin Lin <moneycat711@gmail.com>
2019-12-15 19:29:09 +0800

    Rename to register-user.dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { Repository } from 'typeorm';
-import { RegisterUserDto } from './dto/register_user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
 export class UsersService {
