470b4ba0d5e03ce760b2b793641e9b6fd132df07
virtualprism <alan159753@gmail.com>
2019-12-16 18:09:17 +0800

    Remove edit-profile.dto.ts

--- src/users/dto/edit-profile.dto.ts
+++
@@ -1,9 +0,0 @@
-import { MaxLength } from 'class-validator';
-
-export class RegisterUserDto {
-  @MaxLength(180)
-  readonly nickname: string = '';
-
-  @MaxLength(250)
-  readonly biology: string = '';
-}
