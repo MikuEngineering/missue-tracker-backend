311d7cff0a290ac80ba58e5aed9917dde413c177
virtualprism <alan159753@gmail.com>
2019-12-16 18:38:10 +0800

    Add 'IsString' decorator to the fields and error messages

--- src/users/dto/update-profile.dto.ts
+++ src/users/dto/update-profile.dto.ts
@@ -1,9 +1,19 @@
-import { MaxLength } from 'class-validator';
+import { MaxLength, IsString } from 'class-validator';
 
 export class UpdateProfileDto {
-  @MaxLength(180)
-  readonly nickname: string = '';
+  @IsString({
+    message: 'The type of nickname must be string.'
+  })
+  @MaxLength(180, {
+    message: 'The length of nickname must be less than or equal to 180.'
+  })
+  readonly nickname: string;
 
-  @MaxLength(250)
-  readonly autobiography: string = '';
+  @IsString({
+    message: 'The type of nickname must be string.'
+  })
+  @MaxLength(250, {
+    message: 'The length of autobiography must be less than or equal to 250.'
+  })
+  readonly autobiography: string;
 }
