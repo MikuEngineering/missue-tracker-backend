d5e78b51c15d51dc9faae89b0f59fb69fbb7fb76
virtualprism <alan159753@gmail.com>
2019-12-22 14:56:05 +0800

    Fix the type name of IdValidationPipe

--- src/common/pipes/id-validation.pipe.ts
+++ src/common/pipes/id-validation.pipe.ts
@@ -1,6 +1,7 @@
 import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
 import { ErrorRecord } from '../util/error-record-generator';
 
+const TYPE_NAME = 'parameter';
 const FIELD_NAME_ID = 'id';
 const CODE_NAME = 'isInt';
 
@@ -8,14 +9,14 @@ const REGEX_VALIDATOR = /^\d+$/;
 
 @Injectable()
 export class IdValidationPipe implements PipeTransform<any> {
-  async transform(value: any, { metatype, type }: ArgumentMetadata) {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
     if (!metatype || metatype !== Number) {
       return value;
     }
 
     if (!REGEX_VALIDATOR.test(value)) {
       const errorRecord: ErrorRecord = {
-        type,
+        type: TYPE_NAME,
         field: [FIELD_NAME_ID],
         code: CODE_NAME,
         message: 'The parameter id should be an integer.',
