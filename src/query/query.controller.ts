9f5a70f217556665f4e95a16bb93338412074032
virtualprism <alan159753@gmail.com>
2019-12-24 21:55:28 +0800

    Search issues by title

--- src/query/query.controller.ts
+++ src/query/query.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Query, Request } from '@nestjs/common';
 import { Request as ExpressRequest } from 'express';
 import { QueryProjectDto } from './dto/query-project.dto';
+import { QueryIssueDto } from './dto/query-issue.dto';
 import { ProjectsService } from '../projects/projects.service';
+import { IssuesService } from '../issues/issues.service';
 import { Permission } from '../users/users.entity';
 import { SessionUser } from '../common/types/session-user.type';
 
@@ -9,6 +11,7 @@ import { SessionUser } from '../common/types/session-user.type';
 export class QueryController {
   constructor(
     private readonly projectsService: ProjectsService,
+    private readonly issuesService: IssuesService,
   ) { }
 
   @Get('projects')
@@ -29,5 +32,25 @@ export class QueryController {
       );
     }
     return { projects: projectIds };
+  }
+
+  @Get('issues')
+  async queryIssues(
+    @Query() queryIssueDto: QueryIssueDto,
+    @Request() request: ExpressRequest,
+  ) {
+    const user: SessionUser | undefined = request.user as SessionUser;
+    const userId: number | undefined = user && user.id;
+    const permission: Permission | undefined = user && user.permission;
+    const title: string | undefined = queryIssueDto.title;
+    let issueIds: number[] = [];
+    if (typeof title === 'string') {
+      issueIds = await this.issuesService.searchManyByTitle(
+        title,
+        userId,
+        permission,
+      );
+    }
+    return { issues: issueIds };
   }
 }
--- src/query/query.module.ts
+++ src/query/query.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueryController } from './query.controller';
 import { ProjectsModule } from '../projects/projects.module';
+import { IssuesModule } from '../issues/issues.module';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, IssuesModule],
   controllers: [QueryController],
 })
 export class QueryModule {}
